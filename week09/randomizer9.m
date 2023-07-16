93c84c512b67b771948c371ba900df3a43078d70
parince <konrad.tupta@gmail.com>
2023-07-16 22:46:24 +0200

    Added week 9 randomizer

---
+++ week09/randomizer9.m
@@ -0,0 +1,14 @@
+%Dane do punktu 1
+K_sn=(1+randi(4,1))
+a_sn=(10+randi(20,1))/10
+K_n=(1+randi(4,1))
+B_n=(1+randi(4,1))
+K_nsn=(1+randi(4,1))
+a_nsn=(10+randi(20,1))/10
+B_nsn=(1+randi(4,1))
+%Dane do punktu 2 (k3, k4 i a1 = a również do punktu 3)
+k1=(1+randi(4,1))
+k2=k1+(10+randi(20,1))/10
+k3=(1+randi(4,1))
+k4=k3-(1+randi(9,1))/10
+a=(1+randi(25,1))/10
