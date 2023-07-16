5d351ca1afdade8466b50766013f50b26a3883fb
parince <konrad.tupta@gmail.com>
2023-07-16 22:47:01 +0200

    Added week 10 randomizer

---
+++ week10/randomizer10.m
@@ -0,0 +1,11 @@
+%Dane do punktu 1
+B=(1+randi(3,1))
+a1=(1+randi(7,1))/100
+a2=a1+(10+randi(20,1))/1000
+%Dane do punktu 2
+tau=(1+randi(15,1))/100
+K=(2+randi(3,1))/10
+Tm=(4+randi(3,1))/10
+Tt1=Tm-(1+randi(2,1))/10
+Tt2=Tm
+Tt3=Tm+(1+randi(5,1))/10
